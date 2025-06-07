de573b2d29b2315e04708ffbd921dd211f115e2b
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-06 23:09:51 -0500

    Se añadio un bucle 'do' para comprobar si el valor es 0 o menor

--- ejercicio_13.cpp
+++ ejercicio_13.cpp
@@ -1,29 +1,27 @@
-//En un hospital existen tres áreas: Ginecología, Pediatría, Traumatología//
-//El presupuesto anual del hospital se reparte conforme a la sig. tabla://
-//Área Porcentaje del presupuesto//
-//Ginecología 40%//
-//Traumatología 30%//
-//Pediatría 30%//
-//Obtener la cantidad de dinero que recibirá cada área//
-//para cualquier monto presupuestal dado//
-
 #include<iostream>
 
 using namespace std;
 
 int main(){
 	int P, PG, PT, PP;
-	
-	cout<<"ingrese el presupuesto del hospital:"<<endl;
-	cin>>P;
+		
+	do{
+		cout<<"Ingrese el presupuesto del hospital:"<<endl;
+		cin>>P;
+		
+		if(P<=0){
+		cout<<"El presupuesto no puede ser cero o menor...!!!"<<endl;
+		}
+		
+	}while(P<=0);
 	
 	PG = P * 0.40;
 	PT = P * 0.30;
 	PP = P * 0.30;
 	
-	cout<<"el presupuesto para ginecolgia es de: S/."<<PG<<endl;
-	cout<<"el presupuesto para traumatologia es de: S/."<<PT<<endl; 
-	cout<<"el presupuesto para pedriatria es de: S/."<<PP<<endl;
+	cout<<"El presupuesto para ginecolgia es de: S/."<<PG<<endl;
+	cout<<"El presupuesto para traumatologia es de: S/."<<PT<<endl; 
+	cout<<"El presupuesto para pedriatria es de: S/."<<PP<<endl;
 	
 	return 0;
 	
