57f979ae324d74fd44d9c7bfd0d21258bb07d252
Madi <mayradiazchambilla327@gmail.com>
2025-06-07 20:00:42 -0500

    Modifique la estructura general del ejercicio de tal generando una funcion en su desarrollo

--- P1.cpp
+++ P1.cpp
@@ -1,20 +1,33 @@
 #include<iostream>
 using namespace std;
+int encontrarMayor(int Num1, int Num2, int Num3) {
+    if (Num1>Num2 && Num1>Num3){
+        return Num1;
+        
+    }else if (Num2>Num1 && Num2>Num3){
+        return Num2;
+        
+    } else {
+        return Num3;
+
+    }
+}
+
 
 int main() {
-	double Num1,Num2,Num3,M;
-	cout<<"Ingrese el numero 1: ";
-	cin>>Num1;
-	cout<<"Ingrese el numero 2: ";
-	cin>>Num2;
-	cout<<"Ingrese el numero 3: ";
-	cin>>Num3;
-	if (Num1>Num2>Num3){
-		cout << "El mayor es:"<<Num1<< endl;
-	}else if (Num2>Num1>Num3){
-		cout << "El mayor es:"<<Num2<< endl;
-	}else { (Num3>Num1>Num2); 
-		cout << "El mayor es:"<<Num3<< endl;
-	}
+    int Num1,Num2,Num3;
+    cout <<"Bienvenido a mi programa" <<endl;
+    
+    cout <<"Ingresa un numero: " ;
+    cin >> Num1;
+    cout <<"Ingresa un numero: " ;
+    cin>> Num2;
+    cout <<"Ingresa un numero: ";
+    cin>> Num3;
+    
+    int mayor = encontrarMayor(Num1, Num2, Num3);
+    
+    cout << "El mayor numero es: " << mayor << endl;
+    cout << "Gracias por utilizar el programa " << mayor << endl;
     return 0;
 }
