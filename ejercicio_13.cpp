f8cb217e283a9b694411067039d4a5bfa17a7bb2
maricielo <cielotapia198@gmail.com>
2025-06-08 20:55:32 -0500

    mejore la interfaz del usuario, modifiqué las varariables para que fueran más descriptivas y también las cambie el tipo de variable int a double para mayor precisión.

--- ejercicio_13.cpp
+++ ejercicio_13.cpp
@@ -3,25 +3,34 @@
 using namespace std;
 
 int main(){
-	int P, PG, PT, PP;
+	double Presupuesto, Presu_Gineco, Presu_Trauma, Presu_Pedria;
+	
+	cout << "===========================================" << endl;
+	
+	cout << "\n BIENVENIDO A MI PROGRAMA \n" << endl;
 		
 	do{
-		cout<<"Ingrese el presupuesto del hospital:"<<endl;
-		cin>>P;
+		cout << "Ingrese el presupuesto del hospital:" << endl;
+		cin >> Presupuesto;
 		
-		if(P<=0){
-		cout<<"El presupuesto no puede ser cero o menor...!!!"<<endl;
+		if(Presupuesto <= 0){
+		cout<<"\nEl presupuesto no puede ser cero o menor...!!! \n" <<endl;
 		}
+	
+	} while(Presupuesto <= 0);	
 		
-	}while(P<=0);
+		
+	Presu_Gineco = Presupuesto * 0.40;
+	Presu_Trauma = Presupuesto * 0.30;
+	Presu_Pedria = Presupuesto * 0.30;
 	
-	PG = P * 0.40;
-	PT = P * 0.30;
-	PP = P * 0.30;
+	cout << "\nEl presupuesto para ginecolgia es de: S/." << Presu_Gineco << endl;
 	
-	cout<<"El presupuesto para ginecolgia es de: S/."<<PG<<endl;
-	cout<<"El presupuesto para traumatologia es de: S/."<<PT<<endl; 
-	cout<<"El presupuesto para pedriatria es de: S/."<<PP<<endl;
+	cout << "El presupuesto para traumatologia es de: S/." << Presu_Trauma << endl; 
+	
+	cout << "El presupuesto para pedriatria es de: S/." << Presu_Pedria <<endl;
+	
+	cout << "\n GRACIAS POR USAR MI PROGRAMA <3!";
 	
 	return 0;
 	
